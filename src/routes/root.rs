c8829454c4c7821bcaeb47123c825f29fe3a8f83
Miguel Piedrafita <github@miguel.build>
2023-05-19 01:05:07 +0200

    Return app info on /

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,3 +1,19 @@
-pub async fn get() -> String {
-    "".to_string()
+use std::env;
+
+use axum::Json;
+
+#[derive(Debug, serde::Serialize)]
+pub struct AppVersion {
+    semver: String,
+    rev: Option<String>,
+    compile_time: String,
+}
+
+#[allow(clippy::unused_async)]
+pub async fn get() -> Json<AppVersion> {
+    Json(AppVersion {
+        rev: env::var("GIT_REV").ok(),
+        semver: env!("CARGO_PKG_VERSION").to_string(),
+        compile_time: env!("STATIC_BUILD_DATE").to_string(),
+    })
 }
