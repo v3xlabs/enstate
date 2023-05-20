329dbfa6c7ebc13d06d5186285d38116664fc3b4
Miguel Piedrafita <github@miguel.build>
2023-05-20 02:24:03 +0200

    Convert Redis Set to SetEx (#4)

--- src/models/profile.rs
+++ src/models/profile.rs
@@ -35,8 +35,7 @@ impl Profile {
 
                     if let ProviderError::EnsError(_) = error {
                         // Cache the value, and expire it after 5 minutes
-                        redis.set::<_, _, ()>(&cache_key, "").await.unwrap();
-                        redis.expire::<_, ()>(&cache_key, 300).await.unwrap();
+                        redis.set_ex::<_, _, ()>(&cache_key, "", 300).await.unwrap();
                     };
 
                     return Err(Error::NotFound);
@@ -44,8 +43,10 @@ impl Profile {
             };
 
             // Cache the value, and expire it after 5 minutes
-            redis.set::<_, _, ()>(&cache_key, &result).await.unwrap();
-            redis.expire::<_, ()>(&cache_key, 300).await.unwrap();
+            redis
+                .set_ex::<_, _, ()>(&cache_key, &result, 300)
+                .await
+                .unwrap();
 
             result
         };
@@ -96,8 +97,10 @@ impl Profile {
 
         let response = serde_json::to_string(&value).unwrap();
 
-        redis.set::<_, _, ()>(&cache_key, &response).await.unwrap();
-        redis.expire::<_, ()>(&cache_key, 300).await.unwrap();
+        redis
+            .set_ex::<_, _, ()>(&cache_key, &response, 300)
+            .await
+            .unwrap();
 
         Ok(value)
     }
