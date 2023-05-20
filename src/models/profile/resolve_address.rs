fe559258b8955b791a1134ac083392ea3227a5b8
Luc <luc@lucemans.nl>
2023-05-20 03:24:41 +0000

    Migrate Resolve Address

---
+++ src/models/profile/resolve_address.rs
@@ -0,0 +1,15 @@
+use ethers::{providers::Middleware, types::H160};
+
+use crate::{models::profile::Profile, state::AppState};
+
+use super::ProfileError;
+
+impl Profile {
+    pub async fn resolve_address(name: &str, state: &AppState) -> Result<H160, ProfileError> {
+        Ok(state.provider.resolve_name(name).await.map_err(|e| {
+            println!("Error resolving name: {e:?}");
+
+            ProfileError::NotFound
+        })?)
+    }
+}
