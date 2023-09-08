d50548feb38e21127b299f73abe548cbd213080d
Luc <luc@lucemans.nl>
2023-09-08 17:05:06 +0000

    x

--- src/models/lookup/avatar.rs
+++ src/models/lookup/avatar.rs
@@ -1,9 +1,11 @@
 use super::*;
 use ethers_core::abi::{ParamType, Token};
 use hex_literal::hex;
+use tracing::info;
 
 pub struct Avatar {
     ipfs_gateway: String,
+    name: String,
 }
 
 impl Avatar {}
@@ -26,6 +28,57 @@ impl ENSLookup for Avatar {
         let value = decoded_abi.get(0).ok_or(ENSLookupError::AbiError)?;
         let value = value.to_string();
 
+        // If IPFS
+        let ipfs = regex::Regex::new(r"ipfs://([0-9a-zA-Z]+)").unwrap();
+        if let Some(captures) = ipfs.captures(&value) {
+            let hash = captures.get(1).unwrap().as_str();
+
+            return Ok(Some(format!("{}{hash}", self.ipfs_gateway)));
+        }
+
+        // If the raw value is eip155 url
+        let eip155 =
+            regex::Regex::new(r"eip155:([0-9]+)/(erc1155|erc712):0x([0-9a-fA-F]{40})/([0-9]+)")
+                .unwrap();
+
+        if let Some(captures) = eip155.captures(&value) {
+            let chain_id = captures.get(1).unwrap().as_str();
+            let contract_type = captures.get(2).unwrap().as_str();
+            let contract_address = captures.get(3).unwrap().as_str();
+            let token_id = captures.get(4).unwrap().as_str();
+
+            info!(
+                "Encountered Avatar: {chain_id} {contract_type} {contract_address} {token_id}",
+                chain_id = chain_id,
+                contract_type = contract_type,
+                contract_address = contract_address,
+                token_id = token_id
+            );
+
+            // TODO: Remove naive approach
+            return Ok(Some(format!(
+                "https://metadata.ens.domains/mainnet/avatar/{}",
+                self.name
+            )));
+        }
+
         Ok(Some(value))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ethers::providers::namehash;
+
+    #[tokio::test]
+    async fn test_calldata_avatar() {
+        assert_eq!(
+            Avatar{
+                ipfs_gateway: "https://ipfs.io/ipfs/".to_string(),
+                name: "luc.eth".to_string(),
+            }.calldata(&namehash("luc.eth")),
+            hex_literal::hex!("59d1d43ce1e7bcf2ca33c28a806ee265cfedf02fedf1b124ca73b2203ca80cc7c91a02ad000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000066176617461720000000000000000000000000000000000000000000000000000")
+        );
     }
 }
