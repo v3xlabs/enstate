bd6f976a082f36519054498ab85f0285ade32ea1
Antony1060 <antoniostignjedec@gmail.com>
2023-09-11 15:19:55 +0200

    Implement BNB, Cardano & Tezos address decoding

--- src/models/multicoin/decoding/binance.rs
+++ src/models/multicoin/decoding/binance.rs
@@ -1,11 +1,13 @@
+use bech32::{ToBase32, Variant};
+
 use super::{MulticoinDecoder, MulticoinDecoderError};
 
 pub struct BinanceDecoder {}
 
 impl MulticoinDecoder for BinanceDecoder {
     fn decode(&self, data: &[u8]) -> Result<String, MulticoinDecoderError> {
-        _ = data;
-        Err(MulticoinDecoderError::NotSupported)
+        bech32::encode("bnb", data.to_base32(), Variant::Bech32)
+            .map_err(|_| MulticoinDecoderError::InvalidStructure("failed to bech32 encode".to_string()))
     }
 }
 
--- src/models/multicoin/decoding/tezos.rs
+++ src/models/multicoin/decoding/tezos.rs
@@ -1,12 +1,59 @@
+use bs58::Alphabet;
+
+use crate::utils;
+
 use super::{MulticoinDecoder, MulticoinDecoderError};
 
 pub struct TezosDecoder {}
 
+const CONTRACT_PREFIX: &[u8; 3] = &[0x02, 0x5a, 0x79];
+
 impl MulticoinDecoder for TezosDecoder {
     fn decode(&self, data: &[u8]) -> Result<String, MulticoinDecoderError> {
-        _ = data;
-        Err(MulticoinDecoderError::NotSupported)
+        if data.len() != 21 && data.len() != 22 {
+            return Err(MulticoinDecoderError::InvalidStructure("invalid address length".to_string()));
+        }
+
+        let prefix: &[u8; 3] = match data[0] {
+            0x00 => match data[1] {
+                0x00 => Ok(&[0x06, 0xa1, 0x9f]),
+                0x01 => Ok(&[0x06, 0xa1, 0xa1]),
+                0x02 => Ok(&[0x06, 0xa1, 0xa4]),
+                0x03 => Ok(&[0x06, 0xa1, 0xa6]),
+                _ => Err(MulticoinDecoderError::InvalidStructure("invalid address format".to_string()))
+            },
+            0x01 => Ok(CONTRACT_PREFIX),
+            _ => Err(MulticoinDecoderError::InvalidStructure("invalid address type".to_string()))
+        }?;
+
+        let decoded = [
+            prefix as &[u8],
+            match prefix {
+                CONTRACT_PREFIX => &data[1..data.len() - 1],
+                _ => &data[2..]
+            }
+        ].concat();
+
+        let checksum = utils::sha256::hash(utils::sha256::hash(decoded.clone()));
+
+        Ok(
+            bs58::encode([&decoded, &checksum[..4]].concat())
+                .with_alphabet(Alphabet::BITCOIN)
+                .into_string()
+        )
     }
 }
 
 // TODO: tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_a() {
+        println!("{:?}", TezosDecoder {}.decode(&[1, 227, 201, 210, 54, 50, 195, 66, 162, 220, 156, 147, 249, 139, 79, 211, 149, 194, 64, 36, 222, 0]
+        ));
+
+        assert_eq!(1, 1);
+    }
+}
