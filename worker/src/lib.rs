a2fe28ca8d40bf9990c413ad2bb57623d9af9e27
Luc <luc@lucemans.nl>
2023-10-21 17:12:48 +0000

    x

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -9,38 +9,38 @@ mod kv_cache;
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
     let router = Router::new();
+
     let rpc = Provider::<Http>::try_from(
         "https://rpc.ankr.com/eth/17c7bd60d262bc06008f0a111ca740955ee09d4bb33ecb57d2700214c8f625f1",
     )
     .unwrap();
 
-    // let kv = env.kv("enstate").unwrap();
-
     let cache = Box::new(CloudflareKVCache::new());
     let profile_records = Records::default().records;
     let profile_chains = Coins::default().coins;
 
-    router
-        .get_async("/n/:name", |_req, ctx| async move {
-            if let Some(name) = ctx.param("name") {
-                println!("name: {}", name);
+    // router
+    //     .get_async("/n/:name", |_req, ctx| async move {
+    //         if let Some(name) = ctx.param("name") {
+    //             println!("name: {}", name);
 
-                // match Profile::from_name(name, false, cache, rpc, profile_records, profile_chains)
-                //     .await
-                // {
-                //     Ok(data) => {
-                //         // let json = serde_json::to_string(&data).unwrap();
+    //             // match Profile::from_name(name, false, cache, rpc, profile_records, profile_chains)
+    //             //     .await
+    //             // {
+    //             //     Ok(data) => {
+    //             //         // let json = serde_json::to_string(&data).unwrap();
 
-                //         // Response::from_json(json).unwrap()
-                //         return Response::ok("Hello World");
-                //     }
-                //     Err(e) => Response::error(e.to_string(), 500),
-                // }
-                return Response::ok("Hello World");
-            }
+    //             //         // Response::from_json(json).unwrap()
+    //             //         return Response::ok("Hello World");
+    //             //     }
+    //             //     Err(e) => Response::error(e.to_string(), 500),
+    //             // }
+    //             return Response::ok("Hello World");
+    //         }
 
-            Response::error("Bad Request", 400)
-        })
-        .run(req, env)
-        .await
+    //         Response::error("Bad Request", 400)
+    //     })
+    //     .run(req, env)
+    //     .await
+    Response::ok("Hello World")
 }
