f5925cb11e4658ce0dfb3181af5dd3417dd0cad4
Luc <luc@lucemans.nl>
2023-10-22 02:47:03 +0000

    x

--- shared/src/cache/mod.rs
+++ shared/src/cache/mod.rs
@@ -5,8 +5,17 @@ pub enum CacheError {
     Other(String),
 }
 
+#[cfg(target_arch = "wasm32")]
 #[async_trait(?Send)]
 pub trait CacheLayer: Send + Sync {
     async fn get(&self, key: &str) -> Result<String, CacheError>;
     async fn set(&self, key: &str, value: &str, expires: u32) -> Result<(), CacheError>;
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+#[async_trait]
+pub trait CacheLayer: Send + Sync {
+    async fn get(&self, key: &str) -> Result<String, CacheError>;
+    async fn set(&self, key: &str, value: &str, expires: u32) -> Result<(), CacheError>;
+}
+
