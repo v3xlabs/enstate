8fde433bbc94c9507db681505bb5ccf410908a7c
Antony1060 <antoniostignjedec@gmail.com>
2023-11-16 23:32:52 +0100

    Bump version, code cleanup & introduce root endpoint for worker

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use std::sync::Arc;
 
 use worker::{event, Context, Cors, Env, Method, Request, Response};
@@ -8,11 +9,25 @@ mod http_util;
 mod kv_cache;
 mod lookup;
 
-#[event(fetch, respond_with_errors)]
-async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Response> {
-    let cors = Cors::default()
+lazy_static! {
+    static ref CORS: Cors = Cors::default()
         .with_origins(vec!["*"])
         .with_methods(Method::all());
+}
+
+#[derive(Debug, serde::Serialize)]
+struct AppVersion {
+    rev: String,
+    name: String,
+    semver: String,
+    compile_time: String,
+}
+
+#[event(fetch, respond_with_errors)]
+async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Response> {
+    if req.path() == "/" {
+        return root_handler().with_cors(&CORS);
+    }
 
     let opensea_api_key = env
         .var("OPENSEA_API_KEY")
@@ -26,5 +41,15 @@ async fn main(req: Request, env: Env, _ctx: Context) -> worker::Result<Response>
         .await
         .unwrap_or_else(|f| f);
 
-    response.with_cors(&cors)
+    response.with_cors(&CORS)
+}
+
+fn root_handler() -> Response {
+    Response::from_json(&AppVersion {
+        rev: env!("GIT_REV").to_string(),
+        name: env!("CARGO_PKG_NAME").to_string(),
+        semver: env!("CARGO_PKG_VERSION").to_string(),
+        compile_time: env!("STATIC_BUILD_DATE").to_string(),
+    })
+    .expect("from_json should've succeeded")
 }
