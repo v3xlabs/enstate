474dd690e45049673235ea5d0a60ce041791c39c
Luc <luc@lucemans.nl>
2023-12-11 03:53:02 +0000

    x

--- shared/src/cache/mod.rs
+++ shared/src/cache/mod.rs
@@ -13,3 +13,17 @@ pub trait CacheLayer: Send + Sync {
     async fn get(&self, key: &str) -> Result<String, CacheError>;
     async fn set(&self, key: &str, value: &str, expires: u32) -> Result<(), CacheError>;
 }
+
+pub struct PassthroughCacheLayer {}
+
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl CacheLayer for PassthroughCacheLayer {
+    async fn get(&self, _key: &str) -> Result<String, CacheError> {
+        Err(CacheError::Other("".to_string()))
+    }
+
+    async fn set(&self, _key: &str, _value: &str, _expires: u32) -> Result<(), CacheError> {
+        Ok(())
+    }
+}
