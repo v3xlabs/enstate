73cdb02f3936c50b56b75be82ab0abc72b66b259
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-05-12 19:30:43 +0200

    Introduce POST method for SSE (#109)

--- server/src/routes/universal.rs
+++ server/src/routes/universal.rs
@@ -14,14 +14,15 @@ use enstate_shared::core::{ENSService, Profile};
 use futures::future::join_all;
 use serde::Deserialize;
 use tokio_stream::wrappers::UnboundedReceiverStream;
-use utoipa::IntoParams;
+use utoipa::openapi::schema;
+use utoipa::{IntoParams, ToSchema};
 
 use crate::models::bulk::{BulkResponse, ListResponse};
 use crate::models::sse::SSEResponse;
 use crate::routes::{profile_http_error_mapper, validate_bulk_input, FreshQuery, Qs, RouteError};
 
 /// /u/{name_or_address}
-/// 
+///
 /// The Universal Endpoint supports looking up both names and addresses.
 ///
 /// Here is an example of a valid request that looks up an address or name:
@@ -29,7 +30,7 @@ use crate::routes::{profile_http_error_mapper, validate_bulk_input, FreshQuery, 
 /// /u/luc.eth
 /// /u/0x225f137127d9067788314bc7fcc1f36746a3c3B5
 /// ```
-/// 
+///
 /// You can also use the [useProfile](https://github.com/v3xlabs/use-enstate/blob/master/src/hooks/useProfile.ts) hook from [use-enstate](https://github.com/v3xlabs/use-enstate).
 #[utoipa::path(
     get,
@@ -64,25 +65,28 @@ pub async fn get(
     })?
 }
 
-#[derive(Deserialize, IntoParams)]
+#[derive(Deserialize, IntoParams, ToSchema)]
 pub struct UniversalGetBulkQuery {
     // TODO (@antony1060): remove when proper serde error handling
+    // list of names or addresses to lookup
+    #[schema(example = json!(["luc.eth", "nick.eth", "helgesson.eth", "irc.eth", "khori.eth", "v3x.eth"]))]
     #[serde(default)]
     queries: Vec<String>,
 
+    #[schema(example = "false")]
     #[serde(flatten)]
     fresh: FreshQuery,
 }
 
 /// /bulk/u
-/// 
+///
 /// The Universal Endpoint supports looking up both names and addresses.
-/// 
+///
 /// Here is an example of a valid request that looks up multiple names:
 /// ```url
 /// /bulk/u?queries[]=luc.eth&queries[]=nick.eth&queries[]=helgesson.eth&queries[]=irc.eth&queries[]=khori.eth&queries[]=v3x.eth
 /// ```
-/// 
+///
 /// You can also use the [useBulkProfile](https://github.com/v3xlabs/use-enstate/blob/master/src/hooks/useBulkProfile.ts) hook from [use-enstate](https://github.com/v3xlabs/use-enstate).
 #[utoipa::path(
     get,
@@ -116,14 +120,14 @@ pub async fn get_bulk(
 }
 
 /// /sse/u
-/// 
+///
 /// The Universal Endpoint supports looking up both names and addresses.
-/// 
+///
 /// Here is an example of a valid request that looks up multiple names:
 /// ```url
 /// /sse/u?queries[]=luc.eth&queries[]=nick.eth&queries[]=helgesson.eth&queries[]=irc.eth&queries[]=khori.eth&queries[]=v3x.eth
 /// ```
-/// 
+///
 /// You can also use the [useSSEProfiles](https://github.com/v3xlabs/use-enstate/blob/master/src/hooks/useSSEProfiles.ts) hook from [use-enstate](https://github.com/v3xlabs/use-enstate).
 #[utoipa::path(
     get,
@@ -172,6 +176,27 @@ pub async fn get_bulk_sse(
 
     Sse::new(UnboundedReceiverStream::new(event_rx))
         .keep_alive(axum::response::sse::KeepAlive::new().interval(Duration::from_secs(1)))
+}
+
+/// /sse/u
+/// 
+/// Same as the GET version, but using POST with a JSON body instead of query parameters allowing for larger requests.
+#[utoipa::path(
+    post,
+    tag = "Stream Profiles",
+    path = "/sse/u",
+    responses(
+        (status = 200, description = "Successfully found name or address.", body = BulkResponse<ENSProfile>),
+        (status = NOT_FOUND, description = "No name or address could be found.", body = ErrorResponse),
+        (status = UNPROCESSABLE_ENTITY, description = "Reverse record not owned by this address.", body = ErrorResponse),
+    ),
+    request_body = UniversalGetBulkQuery,
+)]
+pub async fn post_bulk_sse(
+    State(state): State<Arc<crate::AppState>>,
+    Json(query): Json<UniversalGetBulkQuery>,
+) -> impl IntoResponse {
+    get_bulk_sse(Qs(query), State(state)).await
 }
 
 // helper function for above
