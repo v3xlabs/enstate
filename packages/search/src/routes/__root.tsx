c12b9db6392c3eaed044afa447b48e4ac70f6634
Luc <luc@lucemans.nl>
2025-03-12 23:24:22 +0100

    Update styling

--- packages/search/src/routes/__root.tsx
+++ packages/search/src/routes/__root.tsx
@@ -1,22 +1,23 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { FiSearch } from 'react-icons/fi';
 
 export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen bg-gray-100">
-      <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Search App</h1>
+      <header className="">
+        <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+          <h1 className="text-base font-bold text-gray-900">search enstate</h1>
           <nav className="mt-4">
             <ul className="flex space-x-4">
               <li>
                 <Link 
                   to="/" 
-                  className="text-blue-600 hover:text-blue-800"
+                  className="text-blue-600 hover:text-blue-800 flex items-center gap-2"
                   activeProps={{
                     className: 'text-blue-800 font-bold',
                   }}
                 >
-                  Home
+                  <FiSearch className="w-4 h-4" /> Search
                 </Link>
               </li>
               {/* Search link removed - functionality is now on homepage */}
